40ccf438062cb48ca5746df89a2638b7bcd28e01
sanceli <sanceli@hotmail.com>
2024-07-05 09:16:13 -0500

    Instalacion Argon Laravel Blade Vue template y ajustes

---
+++ resources/views/layouts/footers/nav.blade.php
@@ -0,0 +1,15 @@
+<div class="row align-items-center justify-content-xl-between">
+    <div class="col-xl-6">
+        <div class="copyright text-center text-xl-left text-muted">
+            &copy; {{ now()->year }} <a href="https://blockchain.sanceli.com" class="font-weight-bold ml-1" target="_blank">Proyecto de tesis Santiago Celi A.</a> |
+            <a href="https://utpl.edu.ec" class="font-weight-bold ml-1" target="_blank">UTPL</a>
+        </div>
+    </div>
+    <div class="col-xl-6">
+        <ul class="nav nav-footer justify-content-center justify-content-xl-end">
+            <li class="nav-item">
+                <a href="http://blockchain.sanceli.com/manual.pdf" class="nav-link" target="_blank">Manual de Usuario</a>
+            </li>
+        </ul>
+    </div>
+</div>
---
+++ resources/views/layouts/navbars/navs/guest.blade.php
@@ -0,0 +1,47 @@
+<nav class="navbar navbar-top navbar-horizontal navbar-expand-md navbar-dark">
+    <div class="container px-4">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-collapse-main" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbar-collapse-main">
+            <!-- Collapse header -->
+            <div class="navbar-collapse-header d-md-none">
+                <div class="row">
+                    <div class="col-6 collapse-close">
+                        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbar-collapse-main" aria-controls="sidenav-main" aria-expanded="false" aria-label="Toggle sidenav">
+                            <span></span>
+                            <span></span>
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <!-- Navbar items -->
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link nav-link-icon" href="{{ route('home') }}">
+                        <i class="ni ni-planet"></i>
+                        <span class="nav-link-inner--text">Dashboard</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link nav-link-icon" href="{{ route('register') }}">
+                        <i class="ni ni-circle-08"></i>
+                        <span class="nav-link-inner--text">Registrarse</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link nav-link-icon" href="{{ route('login') }}">
+                        <i class="ni ni-key-25"></i>
+                        <span class="nav-link-inner--text">Ingresar</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link nav-link-icon" href="{{ route('profile.edit') }}">
+                        <i class="ni ni-single-02"></i>
+                        <span class="nav-link-inner--text">Ver perfil</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
